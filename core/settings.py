cd99fb5733e585fb34e23fb840beaf3466e29296
esgaelramos <esgaelramos@gmail.com>
2022-02-23 21:13:31 -0600

    Create database, connect and select

--- core/settings.py
+++ core/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dj_library',
+        'USER': 'postgres',
+        'PASSWORD': 'SEXYpsql!88',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT': '5432',     
     }
 }
+
 
 
 # Password validation
