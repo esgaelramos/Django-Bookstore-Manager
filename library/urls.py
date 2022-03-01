b9c86cb9e754a9662d979499b3aa138548237e17
esgaelramos <esgaelramos@gmail.com>
2022-02-28 21:22:05 -0600

    Register and create urls app in urls core

---
+++ library/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+# from library import views
+
+app_name='library'
+
+urlpatterns = [
+    # path('', views.home, name='home'),
+
+    # path('hello-world', views.HelloWorld, name='hello-world'),
+]
