7ca0c7810d9f3c8a1be147873012052a1b700375
esgaelramos <esgaelramos@gmail.com>
2022-03-06 07:25:08 -0600

    Create a view home

--- library/views.py
+++ library/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Book, Author, Editorial
+from django.db.models import Q #for consultes
 
-# Create your views here.
+def home(request):
+    search = request.GET.get('buscar') #recuperamos la vista
+
+    booksList = Book.objects.all()
+    authorsList = Author.objects.all()
+    editorialsList = Editorial.objects.all()
+
+    if search:
+        booksList = Book.objects.filter(
+            Q(code__icontains = search) | #Check every model
+            Q(book__icontains = search) |
+            Q(author__first_name__icontains = search) |
+            Q(author__last_name__icontains = search) |
+            Q(editorial__editorial__icontains = search)
+        ).distinct()
+
+
+    return render(request, 'home.html', {'books': booksList, 'authors': authorsList, 'editorials': editorialsList})
+
